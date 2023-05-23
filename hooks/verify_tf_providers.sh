410046f6bf1e49e4ef7dea2fd4b359e913743f52
Simon Lee <slee@bellese.io>
2023-05-23 19:48:29 -0400

    more cleanup, refactored verify script

--- hooks/verify_tf_providers.sh
+++ hooks/verify_tf_providers.sh
@@ -1,5 +1,6 @@
-#!/bin/zsh
-if ! [ $(hcl2json .terraform.lock.hcl | jq -r ".provider.\"registry.terraform.io/hashicorp/aws\"[].hashes[]" | grep --color=never "h1:" | wc -l | xargs) -eq 3 ]; then
+#!/bin/sh
+number_of_platforms=$(hcl2json .terraform.lock.hcl | jq -r ".provider.\"registry.terraform.io/hashicorp/aws\"[].hashes[]" | grep --color=never "h1:" | wc -l | xargs)
+if ! [ $number_of_platforms -eq 3 ]; then
   terraform init > /dev/null 2>&1
   rm -rf .terraform.lock.hcl
   terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64
