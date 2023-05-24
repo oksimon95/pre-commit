c1785af17938a1fa47e71a87c4acfa7a0c3f893d
Simon Lee <slee@bellese.io>
2023-05-24 14:59:10 -0400

    refactored mono service hook

--- hooks/update_mono_service_jenkins.sh
+++ hooks/update_mono_service_jenkins.sh
@@ -1,12 +1,20 @@
 #!/bin/zsh
+if [[ $(command -v rg) == '' ]]; then
+  brew install rg
+fi
 apps=($(ls environments/prod/us-east-1 | rg -v ".log|.hcl"))
 old_apps=$(cat Jenkinsfile | rg -o -U --color=never "(\[)(\n.*)+(\])")
-current_apps="["
-for app in $apps
-do
-  current_apps="${current_apps}\n    \"$app\","
-done
-current_apps="${current_apps}\n]"
-old_jenkinsfile=$(cat Jenkinsfile)
-new_jenkinsfile=${old_jenkinsfile/$old_apps/$current_apps}
-echo $new_jenkinsfile > Jenkinsfile
+old_apps_list=$(echo $old_apps | grep ',' | sed "s/,//g" | sort | xargs)
+
+if [[ $apps != $old_apps_list ]]; then
+  current_apps="["
+  for app in $apps
+  do
+    current_apps="${current_apps}\n    \"$app\","
+  done
+  current_apps="${current_apps}\n]"
+  old_jenkinsfile=$(cat Jenkinsfile)
+  new_jenkinsfile=${old_jenkinsfile/$old_apps/$current_apps}
+  echo $new_jenkinsfile > Jenkinsfile
+  false
+fi
