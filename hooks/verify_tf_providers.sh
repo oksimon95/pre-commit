9a081f462c8cb869391c1743d2ac3d378d9b3c87
Simon Lee <slee@bellese.io>
2023-05-23 20:05:34 -0400

    updated lock update output

--- hooks/verify_tf_providers.sh
+++ hooks/verify_tf_providers.sh
@@ -3,7 +3,6 @@ number_of_platforms=$(hcl2json .terraform.lock.hcl | jq -r ".provider.\"registry
 if ! [ $number_of_platforms -eq 3 ]; then
   terraform init > /dev/null 2>&1
   rm -rf .terraform.lock.hcl
-  terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64 > /dev/null 2>&1
-  echo "Updated terraform lock file providers for multiple platforms"
+  terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64
   false
 fi
