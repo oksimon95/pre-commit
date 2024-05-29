fe23d38a9b521127eb4cc469cf79b9eed6e52539
Simon Lee <slee@bellese.io>
2024-05-29 12:00:18 -0400

    fix(verify_tf_providers): added submodule update

--- hooks/verify_tf_providers.sh
+++ hooks/verify_tf_providers.sh
@@ -2,6 +2,29 @@
 if [[ $(command -v hcl2json) == '' ]]; then
   brew install hcl2json
 fi
+current_path=$(pwd)
+tf_lock_updated=true
+if [ -d 'modules' ]; then
+  local sub_modules=($(ls -d modules/*/))
+  if [[ $sub_modules != '' ]]; then
+    for sub_mod in $sub_modules
+    do
+      cd "$current_path/$sub_mod"
+      providers=($(hcl2json .terraform.lock.hcl | jq -r ".provider | to_entries[] | .key" | xargs))
+      for provider in $providers
+      do
+        number_of_platforms=$(hcl2json .terraform.lock.hcl | jq -r ".provider.\"$provider\"[].hashes[]" | grep --color=never "h1:" | wc -l | xargs)
+        if [ $number_of_platforms -lt 4 ]; then
+          terraform init > /dev/null 2>&1
+          rm -rf .terraform.lock.hcl
+          terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64 -platform=darwin_arm64
+          tf_lock_updated=false
+        fi
+      done
+    done
+  fi
+fi
+cd $current_path
 providers=($(hcl2json .terraform.lock.hcl | jq -r ".provider | to_entries[] | .key" | xargs))
 for provider in $providers
 do
@@ -10,6 +33,7 @@ do
     terraform init > /dev/null 2>&1
     rm -rf .terraform.lock.hcl
     terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64 -platform=darwin_arm64
-    false
+    tf_lock_updated=false
   fi
 done
+eval "$tf_lock_updated"
