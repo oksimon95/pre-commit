351418e3de95539a57ba12fa091cef8286ef1c9c
Simon Lee <slee@bellese.io>
2024-05-29 12:53:00 -0400

    fix(verify_tf_providers): simplified verify_tf_provider for all tflocks

--- hooks/verify_tf_providers.sh
+++ hooks/verify_tf_providers.sh
@@ -4,36 +4,20 @@ if [[ $(command -v hcl2json) == '' ]]; then
 fi
 current_path=$(pwd)
 tf_lock_updated=true
-if [ -d 'modules' ]; then
-  local sub_modules=($(ls -d modules/*/))
-  if [[ $sub_modules != '' ]]; then
-    for sub_mod in $sub_modules
-    do
-      cd "$current_path/$sub_mod"
-      providers=($(hcl2json .terraform.lock.hcl | jq -r ".provider | to_entries[] | .key" | xargs))
-      for provider in $providers
-      do
-        number_of_platforms=$(hcl2json .terraform.lock.hcl | jq -r ".provider.\"$provider\"[].hashes[]" | grep --color=never "h1:" | wc -l | xargs)
-        if [ $number_of_platforms -lt 4 ]; then
-          terraform init > /dev/null 2>&1
-          rm -rf .terraform.lock.hcl
-          terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64 -platform=darwin_arm64
-          tf_lock_updated=false
-        fi
-      done
-    done
-  fi
-fi
-cd $current_path
-providers=($(hcl2json .terraform.lock.hcl | jq -r ".provider | to_entries[] | .key" | xargs))
-for provider in $providers
+local mains=($(find ~+ -name 'main.tf' -not -path '*/.terraform/*' -print | sed "s/main\.tf//g" | xargs))
+for main in $mains
 do
-  number_of_platforms=$(hcl2json .terraform.lock.hcl | jq -r ".provider.\"$provider\"[].hashes[]" | grep --color=never "h1:" | wc -l | xargs)
-  if [ $number_of_platforms -lt 4 ]; then
-    terraform init > /dev/null 2>&1
-    rm -rf .terraform.lock.hcl
-    terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64 -platform=darwin_arm64
-    tf_lock_updated=false
-  fi
+  cd $main
+  providers=($(hcl2json .terraform.lock.hcl | jq -r ".provider | to_entries[] | .key" | xargs))
+  for provider in $providers
+  do
+    number_of_platforms=$(hcl2json .terraform.lock.hcl | jq -r ".provider.\"$provider\"[].hashes[]" | grep --color=never "h1:" | wc -l | xargs)
+    if [ $number_of_platforms -lt 4 ]; then
+      terraform init > /dev/null 2>&1
+      rm -rf .terraform.lock.hcl
+      terraform providers lock -platform=windows_amd64 -platform=darwin_amd64 -platform=linux_amd64 -platform=darwin_arm64
+      tf_lock_updated=false
+    fi
+  done
 done
 eval "$tf_lock_updated"
